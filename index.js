fee28b760c25e6b335870cdf9d54da8150ed041f
jessny <jessnyjoseph@gmail.com>
2021-01-18 13:43:41 -0800

    project name a description questions

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ const promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'username'
+            name: 'username',
             message: 'What is your Github username?',
         },
         {
-
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'project name',
+            message: 'What is your projects name?',
+        },
+        {
+            type: 'input',
+            name: 'project description',
+            message: 'Please write a short description of your project.',
         },
     ]);
 };
