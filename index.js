e78c904ed1cf1d152a190a91674f003b9db94126
jessny <jessnyjoseph@gmail.com>
2021-01-18 13:58:42 -0800

    completion of user prompt questions

--- index.js
+++ index.js
@@ -32,5 +32,25 @@ const promptUser = () => {
             message: 'What kind of license should your project have?',
             choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
         },
+        {
+            type: 'input',
+            name: 'dependencies',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to run tests?',
+        },
+        {
+            type: 'input',
+            name: 'user knowledge',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'dependencies',
+            message: 'What does the user need to know about contributing to the repo?',
+        },
     ]);
 };
